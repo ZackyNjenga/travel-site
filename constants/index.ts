34c3b33cab3113c49d406f0405ee7d979003dc4b
ZackyNjenga <zacharynjenga381@gmail.com>
2023-10-23 14:17:57 +0300

    Talk of Committment

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    {href: '/', key: 'home', label: 'Home'},
+    {href: '/', key: 'how_linking-works', label: 'How Hilink Works'},
+    {href: '/', key: 'services', label: 'Services'},
+    {href: '/', key: 'pricing', label: 'Pricing'},
+    {href: '/', key: 'contact_us', label: 'Contact Us'},
+];
