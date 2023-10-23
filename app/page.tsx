62813a83c190bbf95d3317fcaba92d805a58b5a5
ZackyNjenga <zacharynjenga381@gmail.com>
2023-10-23 17:25:05 +0300

    Talk of Committment

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import GetApp from "@/components/GetApp";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-700 text-2xl">
-      Amristar
-    </h1>
+    <>
+        <Hero/>
+        <Camp/>
+        <Guide/>
+        <GetApp/>
+        <Features/>
+    </>
   )
 }
