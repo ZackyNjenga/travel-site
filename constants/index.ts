cb97b03a9efb184d2c878a91795309b87c1d7a44
ZackyNjenga <zacharynjenga381@gmail.com>
2023-10-23 14:38:50 +0300

    Talk of Committment

--- constants/index.ts
+++ constants/index.ts
@@ -6,3 +6,79 @@ export const NAV_LINKS = [
     {href: '/', key: 'pricing', label: 'Pricing'},
     {href: '/', key: 'contact_us', label: 'Contact Us'},
 ];
+
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+]
+
+// FEATURE SECTION
+export const FEATURES =[
+    {
+        title: 'Real maps can be online',
+        icon: '/map.svg',
+        variant: 'green',
+        description: 'What we are offering you is a way to use your most important applications even when offline, even when there is no location at your location',
+    },
+    {
+        title: 'What is your adventure schedule?',
+        icon: '/calendar.svg',
+        variant: 'green',
+        description: 'You want to go on an adventure with friends. That is the best way to enjoy a holiday. We have a ton of interesting offers from HiLink. This is a large choice of camping choices around you',
+
+    },
+    {
+        title: 'Technology using augmented reality',
+        icon: '/tech.svg',
+        variant: 'green',
+        description: 'You can rumble around your adventure or you can use Augmented reality as your guide. It guides you through a forest to the top of mountains. Supported by the latest technology to make sure you are not lost you enjoy yourself and that it doesnt die on you while you are hiking',
+    },
+    {
+        title: 'Lots of Locations available',
+        icon: '/location.svg',
+        variant: 'green',
+        description: 'We keep discovering new and exciting places you can go to and enjoy the outdoors. We add them every new month so you are not travelling to the same place every time you decide to up and leave ',
+    },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+    {
+        title: 'Learn More',
+        links: [
+            'About HiLink',
+            'Press Releases',
+            'Environment',
+            'Jobs',
+            'Privacy Policy',
+            'Contact Us',
+        ],
+    },
+    {
+        title: 'Our Community',
+        links: ['Climbing', 'Hiking hilink', 'Hilink kinthill'],
+    },
+];
+
+export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+        {label: 'Admin Officer', value: '0796723162'},
+        {label: 'Email for equiries', value: 'info@hilinktours.com'}
+    ],
+};
+
+export const SOCIALS = {
+    title: 'Social',
+    links: [
+        '/facebook,svg',
+        '/instagram.svg',
+        '/twitter.svg',
+        '/youtube.svg',
+        '/wordpress.svg',
+    ],
+};
