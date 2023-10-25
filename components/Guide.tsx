11c2e8cb92d7bb50c3d1a7aab78a76ed9c241941
ZackyNjenga <zacharynjenga381@gmail.com>
2023-10-25 13:56:29 +0300

    With some kinks

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Guide = () => {
     return (
         <section className="flexCenter flex-col">
-            <div className="border-2 border-red-500 padding-conatainer max-container w-full pb-32">
+            <div className="padding-container max-container w-full pb-32">
                 <Image src="/camp.svg" alt="camp" width={50} height={50}/>
                 <p className="uppercase regular-30 -mt-1 mb-3 text-green-50">
                     We exist for your excitement
@@ -17,9 +17,41 @@ const Guide = () => {
                         a guide to the places you have heard of. Some you haven't heard of.
                         But with guide you are about to get there. Well virtually for now
                         but that is what we will do. Give you a taste and then let you choose
-                        fro  the myriad of choices that we have. Sort of an outdoor places buffet.
+                        fro the myriad of choices that we have. Sort of an outdoor places buffet.
                         Just dive in and you will see.
                     </p>
+                </div>
+            </div>
+            <div className="flexCenter max-container relative w-full">
+                <Image src="/boat.png" alt="rafting"
+                       width={1440}
+                       height={580}
+                       className="w-full object-cover object-center 2xl:rounded-5xl"
+                />
+                <div className="absolute bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border
+                shadow-md md:left-[5%] lg:top-20">
+                    <Image
+                        src="/meter.svg"
+                        alt="meter"
+                        width={16}
+                        height={158}
+                        className="h-full w-auto"
+                    />
+                    <div className="flexBetween flex-col">
+                        <div className="flex w-full flex-col">
+                            <div className="flexBetween w-full">
+                                <p className="regular-16 textgray-20"> Destination</p>
+                                <p className="bold-16 text-green-50"> 58 mins</p>
+                            </div>
+                            <p className="bold-20 mt-2">Sagana Rapids</p>
+                        </div>
+
+                        <div className="flex w-full flex-col">
+
+                            <p className="bold-16 text-green-50"> Get rafting</p>
+                            <h4 className="bold-20 mt-2 whitespace-nowrap">Sagana Rapids Camp</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
